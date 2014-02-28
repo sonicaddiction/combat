71c79ef0bfde4f5b9992393db42d6873483d17b1
Kristofer <kristofer.fagerstrom@like.tv>
2014-03-01 00:19:41 +0100

    fix(tests): use the correct mock framework

--- test/agent.spec.js
+++ test/agent.spec.js
@@ -1,30 +1,26 @@
-var mock = require('ng-di'),
-	getAgent;
+var mock = require('ng-di/mock');
 
 require('../src/agent.js');
 
 describe('agent', function () {
-	beforeEach(function() {
-		getAgent = mock.injector(['combat.agent']).get('getAgent');
-	});
+	beforeEach(mock.module('combat.agent'));
 
-
-	it('should create an agent with a name', function () {
+	it('should create an agent with a name', mock.inject(function (getAgent) {
 		var agent = getAgent('Artemis');
 
 		expect(agent.name).toBe('Artemis');
-	});
+	}));
 
-	it('should be able to have its health set', function () {
+	it('should be able to have its health set', mock.inject(function (getAgent) {
 		var agent = getAgent('Artemis', 10);
 
 		expect(agent.hp).toBe(10);
-	});
+	}));
 
-	it('should have increasing id numbers', function () {
+	it('should have increasing id numbers', mock.inject(function (getAgent) {
 		var agent1 = getAgent('Artemis'),
 			agent2 = getAgent('Beatrice');
 
 		expect(agent1.id).toBeLessThan(agent2.id);
-	});
+	}));
 });
--- test/engine.spec.js
+++ test/engine.spec.js
@@ -1,33 +1,31 @@
-var mock = require('ng-di'),
+var mock = require('ng-di/mock'),
 	getEngine;
 
 require('../src/engine.js');
 
 describe('engine', function () {
-	beforeEach(function() {
-		getEngine = mock.injector(['combat.engine']).get('getEngine');
-	});
+	beforeEach(mock.module('combat.engine'));
 
-	it('should create an object', function () {
+	it('should create an object', mock.inject(function (getEngine) {
 		var engine = getEngine();
 
 		expect(engine).toBeDefined();
-	});
+	}));
 
-	it('should initially contain no agents', function () {
+	it('should initially contain no agents', mock.inject(function (getEngine) {
 		var engine = getEngine(),
 			agents = Object.keys(engine.agents);
 
 		expect(agents.length).toBe(0);
-	});
+	}));
 
-	it('should initially have no queued actions', function () {
+	it('should initially have no queued actions', mock.inject(function (getEngine) {
 		var engine = getEngine();
 
 		expect(engine.actionQueue.length).toBe(0);
-	});
+	}));
 
-	it('should be able to add agents', function () {
+	it('should be able to add agents', mock.inject(function (getEngine) {
 		var engine = getEngine(),
 			mockAgent0 = {
 				id: 0
@@ -40,9 +38,9 @@ describe('engine', function () {
 		engine.addAgent(mockAgent1);
 
 		expect(Object.keys(engine.agents).length).toBe(2);
-	});
+	}));
 
-	it('should not add the same agent twice', function () {
+	it('should not add the same agent twice', mock.inject(function (getEngine) {
 		var engine = getEngine(),
 			mockAgent = {
 				id: 0
@@ -54,18 +52,18 @@ describe('engine', function () {
 		engine.addAgent(mockAgent);
 
 		expect(addSameAgentAgain).toThrow(new Error('Agent already exists'));
-	});
+	}));
 
-	it('should be able to queue actions', function () {
+	it('should be able to queue actions', mock.inject(function (getEngine) {
 		var engine = getEngine();
 
 		engine.queueAction({ foo: 1 });
 		engine.queueAction({ foo: 2 });
 
 		expect(engine.actionQueue.length).toBe(2);
-	});
+	}));
 
-	it('should be able to perform an action in the queue', function () {
+	it('should be able to perform an action in the queue', mock.inject(function (getEngine) {
 		var engine = getEngine(),
 			action = {
 				perform: function () {}
@@ -78,5 +76,5 @@ describe('engine', function () {
 		engine.step();
 
 		expect(action.perform).toHaveBeenCalled();
-	});
+	}));
 });
